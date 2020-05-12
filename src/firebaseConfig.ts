4dc4025e07c6a012fda51c048848e51b2d38508b
Aksels Ledins <aksels.ledins@gmail.com>
2020-05-12 23:16:47 +0800

    add firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDhEabBARc_fLVj68feJnDrJJZ0oTzk29g',
+  authDomain: 'singapore-baguette.firebaseapp.com',
+  databaseURL: 'https://singapore-baguette.firebaseio.com',
+  projectId: 'singapore-baguette',
+  storageBucket: 'singapore-baguette.appspot.com',
+  messagingSenderId: '839804625375',
+  appId: '1:839804625375:web:59b890efc99ca8fb4be46a',
+  measurementId: 'G-54ZWLHLVQB',
+};
+
+export default firebaseConfig;
