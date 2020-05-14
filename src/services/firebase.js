90820d081fb362e05f9518b14a1a876b2091ad97
Aksels Ledins <aksels.ledins@gmail.com>
2020-05-15 01:23:21 +0800

    firebase analytics

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,12 @@
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/analytics';
 
 import firebaseConfig from '../firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 const db = firebase.firestore();
 
 export const getStores = () => {
