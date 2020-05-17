bef943c1cf12f7a8a6fcec423eeb84466b4bc549
Aksels Ledins <aksels.ledins@gmail.com>
2020-05-17 19:22:03 +0800

    display other features & link

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -7,7 +7,7 @@ import Rating from '../Rating';
 import Features from '../Features';
 
 import './style.scss';
-import { Store } from 'src/data';
+import { Feature, Store } from 'src/data';
 
 type Props = { onClick: Function; store: Store; active?: boolean };
 
@@ -31,11 +31,38 @@ export default ({ onClick, store, active }: Props) => (
       </div>
       {store.description && <div className="item-description">{store.description}</div>}
       {store.address && <div className="item-address">{store.address}</div>}
-      <div className="item-hasBaguette">
-        <Icon path="/marker.svg" size={42} gray={!store.hasBaguette} style={{ backgroundPosition: '-2px -4px' }} />
+      <div className="item-feature">
+        <Icon path="/baguette.svg" size={24} gray={!store.hasBaguette} />
         {store.hasBaguette && <div>sells baguettes</div>}
         {!store.hasBaguette && <div>doesn't have baguette</div>}
       </div>
+
+      {store.features instanceof Array && store.features.includes(Feature.Croissant) && (
+        <div className="item-feature" style={{ marginTop: 0 }}>
+          <Icon path="/croissant.svg" size={24} />
+          <div>can buy a croissant</div>
+        </div>
+      )}
+      {store.features instanceof Array && store.features.includes(Feature.Delivery) && (
+        <div className="item-feature" style={{ marginTop: 0 }}>
+          <Icon path="/delivery.svg" size={24} />
+          <div>delivery available</div>
+        </div>
+      )}
+      {store.features instanceof Array && store.features.includes(Feature.EatIn) && (
+        <div className="item-feature" style={{ marginTop: 0 }}>
+          <Icon path="/eatin.svg" size={24} />
+          <div>can eat in</div>
+        </div>
+      )}
+
+      {store.link && (
+        <div className="item-link">
+          <a href={store.link} target="_blank" rel="noreferrer noopener">
+            <Icon path="/link.svg" size={32} />
+          </a>
+        </div>
+      )}
     </div>
     <Features features={store.features} />
   </div>
