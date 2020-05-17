c622fde2e5df48f44b21b6c0f68e5a85f770504f
Aksels Ledins <aksels.ledins@gmail.com>
2020-05-17 12:59:50 +0800

    c:ListItem/Rating: handle new data on rating

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -6,26 +6,29 @@ import Price from '../Price';
 import Features from '../Features';
 
 import './style.scss';
+import { Store } from 'src/data';
 
-type Props = { onClick: Function; store: any; active?: boolean };
+type Props = { onClick: Function; store: Store; active?: boolean };
 
 export default ({ onClick, store, active }: Props) => (
   <div
-    key={`listing-${store.properties.id}`}
-    id={`listing-${store.properties.id}`}
+    key={`listing-${store.id}`}
+    id={`listing-${store.id}`}
     className={`item ${active ? 'active' : ''}`}
     onClick={() => onClick()}
   >
     <div className="main-content">
       <div className="title-container">
-        <h2 className="title">{store.properties.title}</h2>
-        <Approvness approved={store.properties.approved} />
+        <h2 className="title">{store.title}</h2>
+        <Approvness approved={store.approvedLevel} />
       </div>
       <div style={{ display: 'flex', alignContent: 'center', alignItems: 'baseline' }}>
-        <Rating mark={store.properties.rate} />
+        <Rating googleRating={store['google-rating']} ownRating={store['internal-rating']} />
+        {/*
         <Price price={store.properties.price} />
+        */}
       </div>
     </div>
-    <Features features={store.properties.features} />
+    <Features features={store.features} />
   </div>
 );
--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 import Star from 'src/components/Icons/Star';
+import Icon from 'src/components/Icons/GenericIcon';
 
-const Rating = ({ mark }: { mark: number }) => {
+const Rating = ({ ownRating, googleRating }: { ownRating?: number; googleRating?: number }) => {
   return (
-    <div className="rating">
-      {[...Array(Math.round(mark))].map((_, i) => (
-        <Star key={i} full />
-      ))}
-      {[...Array(5 - Math.round(mark))].map((_, i) => (
-        <Star key={i} empty />
-      ))}
+    <div>
+      <div>
+        {ownRating && ownRating >= 0 && ownRating <= 5 && (
+          <div className="rating">
+            {[...Array(Math.round(ownRating))].map((_, i) => (
+              <Star key={i} full />
+            ))}
+            {[...Array(5 - Math.round(ownRating))].map((_, i) => (
+              <Star key={i} empty />
+            ))}
+          </div>
+        )}
+      </div>
+      <div>
+        {googleRating && googleRating >= 0 && googleRating <= 5 && (
+          <div className="rating">
+            {[...Array(Math.round(googleRating))].map((_, i) => (
+              <Icon key={i} path="/google.svg" />
+            ))}
+            {[...Array(5 - Math.round(googleRating))].map((_, i) => (
+              <Icon key={i} path="/google.svg" gray />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
